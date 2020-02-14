1d87a9d8e151850a3967a1e3d0bb1b555bfa7da6
Gary Olson <golsonmke@gmail.com>
2020-02-14 11:07:25 -0600

    init

---
+++ Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Game
+    {
+        Weather weather;
+        Inventory Inventory;
+
+        public Game()
+        {
+            
+
+        }
+        public void RunGame()
+        {
+           
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Weather
+    {
+        int temperature;
+        string forcast;
+        string cloudy;
+        
+
+        public Weather()
+        {
+           
+
+
+           
+        }
+        public void WeatherType()
+        {
+            List<string> forcast = new List<string>();
+            forcast.Add("Cloudy");
+            forcast.Add("Overcast");
+            forcast.Add("rainy");
+            forcast.Add("Hazy");
+
+            var rnd = new Random();
+            Console.WriteLine("Forcast " + forcast[rnd.Next(0, forcast.Count)]);
+
+        }
+        public void Temperature()
+        {
+            Random random = new Random();
+
+        }
+
+
+    }
+}
